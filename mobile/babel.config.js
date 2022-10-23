615d98dec2f0d96349cf2e89a989c737dd02dbaa
Caio Mendes <caiosamarone10@hotmail.com>
2022-10-22 21:23:21 -0300

    adding mobile project

---
+++ mobile/babel.config.js
@@ -0,0 +1,30 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ".",
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+            ".android.js",
+            ".android.tsx",
+            ".ios.js",
+            ".ios.tsx",
+          ],
+          alias: {
+            components: "./src/components",
+            modules: "./src/modules",
+            screens: "./src/screens",
+            utils: "./src/utils",
+          },
+        },
+      ],
+    ],
+  };
+};
